8e51a7df2484fac77417103447a2b183d5df78af
Rafael Molina García <rafmolgar2@alum.us.es>
2024-03-26 19:45:04 +0100

    [Fix]: fixed warnings in booking controller

--- src/main/java/org/springframework/samples/petclinic/booking/BookingRestController.java
+++ src/main/java/org/springframework/samples/petclinic/booking/BookingRestController.java
@@ -8,28 +8,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.samples.petclinic.auth.payload.response.MessageResponse;
-import org.springframework.samples.petclinic.clinicowner.ClinicOwner;
-import org.springframework.samples.petclinic.clinicowner.ClinicOwnerService;
 import org.springframework.samples.petclinic.owner.Owner;
 import org.springframework.samples.petclinic.owner.OwnerService;
-import org.springframework.samples.petclinic.room.Room;
 import org.springframework.samples.petclinic.user.UserService;
 import org.springframework.samples.petclinic.util.RestPreconditions;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/bookings")
 @Tag(name = "Bookings", description = "API for the management of Bookings")
 @SecurityRequirement(name = "bearerAuth")
 public class BookingRestController {
-
-    private static final String VET_AUTH = "VET";
-    private static final String ADMIN_AUTH = "ADMIN";
-    private static final String OWNER_AUTH = "OWNER";
-    private static final String VISIT_CLASSNAME = "Visit";
 
     private final OwnerService ownerService;
     private final BookingService bookingService;
@@ -72,15 +63,12 @@ public class BookingRestController {
     public ResponseEntity<Booking> createRoom(@RequestBody @Valid Booking booking) {
         int petBookingId= booking.getPet().getId();
         int roomBookingId= booking.getRoom().getId();
-        if(!(bookingService.findBookingsByPetId(petBookingId) == null || bookingService.findBookingsByRoomId(roomBookingId) == null)){
+        if(!(bookingService.findBookingsByPetId(petBookingId).isEmpty() || bookingService.findBookingsByRoomId(roomBookingId).isEmpty())){
 
             Booking newBooking = new Booking();
             BeanUtils.copyProperties(booking, newBooking, "id");
             if (booking.getOwner() == null) {
-                Owner owner = ownerService.optFindOwnerByUser(userService.findCurrentUser().getId()).orElse(null);
-                if(owner != null){
-                    booking.setOwner(owner);
-                }
+                ownerService.optFindOwnerByUser(userService.findCurrentUser().getId()).ifPresent(booking::setOwner);
             }
             Booking savedBooking= this.bookingService.saveBooking(newBooking);
 
@@ -93,9 +81,7 @@ public class BookingRestController {
     public ResponseEntity<Booking> updateBooking(@PathVariable("bookingId") int bookingId, @RequestBody @Valid Booking booking){
         int petBookingId= booking.getPet().getId();
         int roomBookingId= booking.getRoom().getId();
-        Booking petBooking= bookingService.findBookingsByPetId(petBookingId).orElseThrow(null);
-        Booking roomBooking = bookingService.findBookingsByRoomId(roomBookingId).orElseThrow(null);
-        if(!(bookingService.findBookingsByPetId(petBookingId) == null || bookingService.findBookingsByRoomId(roomBookingId) == null)){
+        if(!(bookingService.findBookingsByPetId(petBookingId).isEmpty() || bookingService.findBookingsByRoomId(roomBookingId).isEmpty())){
             RestPreconditions.checkNotNull(bookingService.findBookingById(bookingId),"Booking","ID",bookingId);
 
             return new ResponseEntity<>(bookingService.updateBooking(booking,bookingId),HttpStatus.OK);
