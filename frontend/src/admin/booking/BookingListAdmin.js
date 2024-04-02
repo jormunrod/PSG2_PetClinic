a48fb94dcc01aae690d0494bcf52614c15954b1d
Rafael Molina García <rafmolgar2@alum.us.es>
2024-04-02 17:57:46 +0200

    [Fix]: Functionalities where not finished

--- frontend/src/admin/booking/BookingListAdmin.js
+++ frontend/src/admin/booking/BookingListAdmin.js
@@ -29,7 +29,7 @@ export default function BookingListAdmin() {
                 <td>{booking.endDate}</td>
                 <td>{booking.owner.user.username}</td>
                 <td>{booking.pet.type.name}</td>
-                <td>{booking.room.name}</td>
+                <td>{booking.room.name} {booking.room.clinic.name}</td>
                 <td>
                     <ButtonGroup>
                         <Button
@@ -47,7 +47,7 @@ export default function BookingListAdmin() {
                             aria-label={"delete-" + booking.id}
                             onClick={() =>
                                 deleteFromList(
-                                    `/api/v1/bookings/${booking.id}`,
+                                    `/api/bookings/${booking.id}`,
                                     booking.id,
                                     [bookings, setBookings],
                                     [alerts, setAlerts],
