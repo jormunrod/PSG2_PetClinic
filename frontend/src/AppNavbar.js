62cdac4cbfd16ce8f18a469a02e4220d18be13e3
alvaroChico2408 <alvarochicocastellano@gmail.com>
2024-05-06 15:14:52 +0200

    [feat] deleted plan for owners

--- frontend/src/AppNavbar.js
+++ frontend/src/AppNavbar.js
@@ -110,9 +110,6 @@ function AppNavbar() {
                         <NavLink style={{ color: "white" }} tag={Link} to="/consultations">Consultations</NavLink>
                     </NavItem>
                     <NavItem>
-                        <NavLink style={{ color: "white" }} tag={Link} to="/plan">Plan</NavLink>
-                    </NavItem>
-                    <NavItem>
                         <NavLink style={{ color: "white" }} tag={Link} to="/adoptions">Adoptions</NavLink>
                     </NavItem>
                     <NavItem>
