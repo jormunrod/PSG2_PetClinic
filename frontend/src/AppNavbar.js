015d943eb7215d1d8d5ed8385bbf0d97e2157d2e
Jorge Muñoz Rodríguez <jorgemr@protonmail.ch>
2024-05-06 22:04:26 +0200

    [fix]: Navbar

--- frontend/src/AppNavbar.js
+++ frontend/src/AppNavbar.js
@@ -24,16 +24,6 @@ function AppNavbar() {
   const [vet, setVet] = useState({});
   const [message, setMessage] = useState(null);
 
-  const toggleNavbar = () => setCollapsed(!collapsed);
-
-  useEffect(() => {
-    if (jwt) {
-      setRoles(jwt_decode(jwt).authorities);
-      setUsername(jwt_decode(jwt).sub);
-      getPlan();
-    }
-  }, [jwt]);
-
   async function getPlan() {
     if (jwt_decode(jwt).authorities[0] === "OWNER") {
       const myowner = await (
@@ -69,6 +59,16 @@ function AppNavbar() {
       ? plan.charAt(0).toUpperCase() + plan.slice(1).toLowerCase()
       : null;
   }
+
+  const toggleNavbar = () => setCollapsed(!collapsed);
+
+  useEffect(() => {
+    if (jwt) {
+      setRoles(jwt_decode(jwt).authorities);
+      setUsername(jwt_decode(jwt).sub);
+      getPlan();
+    }
+  }, [jwt]);
 
   let adminLinks = <></>;
   let ownerLinks = <></>;
@@ -131,21 +131,37 @@ function AppNavbar() {
               My Pets
             </NavLink>
           </NavItem>
-          <NavItem>
-            <NavLink style={{ color: "white" }} tag={Link} to="/consultations">
-              Consultations
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink style={{ color: "white" }} tag={Link} to="/adoptions">
-              Adoptions
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink style={{ color: "white" }} tag={Link} to="/bookings">
-              Bookings
-            </NavLink>
-          </NavItem>
+          <Feature>
+            <On expression={feature("haveOnlineConsultation")}>
+              <NavItem>
+                <NavLink
+                  style={{ color: "white" }}
+                  tag={Link}
+                  to="/consultations"
+                >
+                  Consultations
+                </NavLink>
+              </NavItem>
+            </On>
+          </Feature>
+          <Feature>
+            <On expression={feature("haveAdoption")}>
+              <NavItem>
+                <NavLink style={{ color: "white" }} tag={Link} to="/adoptions">
+                  Adoptions
+                </NavLink>
+              </NavItem>
+            </On>
+          </Feature>
+          <Feature>
+            <On expression={feature("haveBooking")}>
+              <NavItem>
+                <NavLink style={{ color: "white" }} tag={Link} to="/bookings">
+                  Bookings
+                </NavLink>
+              </NavItem>
+            </On>
+          </Feature>
         </>
       );
     }
@@ -216,6 +232,7 @@ function AppNavbar() {
             Pricing Plans
           </NavLink>
         </NavItem>
+
         <NavItem>
           <NavLink
             style={{ color: "white" }}
@@ -290,285 +307,34 @@ function AppNavbar() {
         </NavItem>
       </>
     );
-
-    roles.forEach((role) => {
-      if (role === "ADMIN") {
-        adminLinks = (
-          <>
-            <NavItem>
-              <NavLink style={{ color: "white" }} tag={Link} to="/owners">
-                Owners
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink style={{ color: "white" }} tag={Link} to="/pets">
-                Pets
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink style={{ color: "white" }} tag={Link} to="/vets">
-                Vets
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                style={{ color: "white" }}
-                tag={Link}
-                to="/consultations"
-              >
-                Consultations
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink style={{ color: "white" }} tag={Link} to="/clinicOwners">
-                Clinic Owners
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink style={{ color: "white" }} tag={Link} to="/clinics">
-                Clinics
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink style={{ color: "white" }} tag={Link} to="/users">
-                Users
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink style={{ color: "white" }} tag={Link} to="/bookings">
-                Bookings
-              </NavLink>
-            </NavItem>
-          </>
-        );
-      }
-      if (role === "OWNER") {
-        ownerLinks = (
-          <>
-            <NavItem>
-              <NavLink style={{ color: "white" }} tag={Link} to="/myPets">
-                My Pets
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink style={{ color: "white" }} tag={Link} to="/plan">
-                Plan
-              </NavLink>
-            </NavItem>
-            <Feature>
-              <On expression={feature("haveOnlineConsultation")}>
-                <NavItem>
-                  <NavLink
-                    style={{ color: "white" }}
-                    tag={Link}
-                    to="/consultations"
-                  >
-                    Consultations
-                  </NavLink>
-                </NavItem>
-              </On>
-            </Feature>
-            <Feature>
-              <On expression={feature("haveAdoption")}>
-                <NavItem>
-                  <NavLink
-                    style={{ color: "white" }}
-                    tag={Link}
-                    to="/adoptions"
-                  >
-                    Adoptions
-                  </NavLink>
-                </NavItem>
-              </On>
-            </Feature>
-            <Feature>
-              <On expression={feature("haveBooking")}>
-                <NavItem>
-                  <NavLink style={{ color: "white" }} tag={Link} to="/bookings">
-                    Bookings
-                  </NavLink>
-                </NavItem>
-              </On>
-            </Feature>
-          </>
-        );
-      }
-      if (role === "VET") {
-        ownerLinks = (
-          <>
-            <NavItem>
-              <NavLink
-                style={{ color: "white" }}
-                tag={Link}
-                to="/consultations"
-              >
-                Consultations
-              </NavLink>
-            </NavItem>
-          </>
-        );
-      }
-
-      if (role === "CLINIC_OWNER") {
-        ownerLinks = (
-          <>
-            <NavItem>
-              <NavLink style={{ color: "white" }} tag={Link} to="/clinics">
-                Clinics
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink style={{ color: "white" }} tag={Link} to="/rooms">
-                Rooms
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink style={{ color: "white" }} tag={Link} to="/owners">
-                Owners
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                style={{ color: "white" }}
-                tag={Link}
-                to="/consultations"
-              >
-                Consultations
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink style={{ color: "white" }} tag={Link} to="/vets">
-                Vets
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                style={{ color: "white" }}
-                tag={Link}
-                to="/customerAgreement"
-              >
-                Customer Agreement
-              </NavLink>
-            </NavItem>
-          </>
-        );
-      }
-    });
-
-    if (!jwt) {
-      publicLinks = (
-        <>
-          <NavItem>
-            <NavLink style={{ color: "white" }} id="docs" tag={Link} to="/docs">
-              Docs
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink
-              style={{ color: "white" }}
-              id="plans"
-              tag={Link}
-              to="/plans"
-            >
-              Pricing Plans
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink
-              style={{ color: "white" }}
-              id="register"
-              tag={Link}
-              to="/register"
-            >
-              Register
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink
-              style={{ color: "white" }}
-              id="login"
-              tag={Link}
-              to="/login"
-            >
-              Login
-            </NavLink>
-          </NavItem>
-        </>
-      );
-    } else {
-      userLinks = (
-        <>
-          <NavItem>
-            <NavLink style={{ color: "white" }} tag={Link} to="/dashboard">
-              Dashboard
-            </NavLink>
-          </NavItem>
-        </>
-      );
-      userLogout = (
-        <>
-          <NavItem>
-            <NavLink style={{ color: "white" }} id="docs" tag={Link} to="/docs">
-              Docs
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink
-              style={{ color: "white" }}
-              id="plans"
-              tag={Link}
-              to="/plans"
-            >
-              Pricing Plans
-            </NavLink>
-          </NavItem>
-          <NavbarText
-            style={{ color: "white" }}
-            className="justify-content-end"
-          >
-            {username}
-          </NavbarText>
-          <NavItem className="d-flex">
-            <NavLink
-              style={{ color: "white" }}
-              id="logout"
-              tag={Link}
-              to="/logout"
-            >
-              Logout
-            </NavLink>
-          </NavItem>
-        </>
-      );
-    }
-
-    return (
-      <div>
-        <Navbar expand="md" dark color="dark">
-          <NavbarBrand href="/">
-            <img
-              alt="logo"
-              src="/logo1-recortado.png"
-              style={{ height: 40, width: 40 }}
-            />
-            PetClinic
-          </NavbarBrand>
-          <NavbarToggler onClick={toggleNavbar} className="ms-2" />
-          <Collapse isOpen={!collapsed} navbar>
-            <Nav className="me-auto mb-2 mb-lg-0" navbar>
-              {userLinks}
-              {adminLinks}
-              {ownerLinks}
-            </Nav>
-            <Nav className="ms-auto mb-2 mb-lg-0" navbar>
-              {publicLinks}
-              {userLogout}
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
-    );
   }
+
+  return (
+    <div>
+      <Navbar expand="md" dark color="dark">
+        <NavbarBrand href="/">
+          <img
+            alt="logo"
+            src="/logo1-recortado.png"
+            style={{ height: 40, width: 40 }}
+          />
+          PetClinic
+        </NavbarBrand>
+        <NavbarToggler onClick={toggleNavbar} className="ms-2" />
+        <Collapse isOpen={!collapsed} navbar>
+          <Nav className="me-auto mb-2 mb-lg-0" navbar>
+            {userLinks}
+            {adminLinks}
+            {ownerLinks}
+          </Nav>
+          <Nav className="ms-auto mb-2 mb-lg-0" navbar>
+            {publicLinks}
+            {userLogout}
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
 }
 
 export default AppNavbar;
